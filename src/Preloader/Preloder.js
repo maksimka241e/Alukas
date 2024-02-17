0191463b52bcac56693e5952df604705d365714e
Maks <maksim11wa11wa@gmail.com>
2024-02-17 13:46:10 +0300

    jtjh

--- src/Preloader/Preloder.js
+++ src/Preloader/Preloder.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function () {
-    var preloader = document.getElementById('preloader');
+    let preloader = document.getElementById('preloader');
     preloader.style.display = 'none';
   });
