2620296bb9fc94bd180163d0468ae9f2b42798bc
Maks <maksim11wa11wa@gmail.com>
2024-02-28 22:13:45 +0300

    fe

---
+++ src/Footer/components/anim.js
@@ -0,0 +1,153 @@
+//Bl1
+function revealFooterBl1(){
+    let reveals = document.querySelectorAll('.FooterContent1Img')
+    for(let i = 0; i < reveals.length;i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 10;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add('active')
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+    
+}
+window.addEventListener('scroll',revealFooterBl1)
+revealFooterBl1()
+//Bl2
+function revealFooterBl2(){
+    let reveals = document.querySelectorAll('.FooterContent1Title')
+    for(let i = 0; i < reveals.length;i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 10;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add('active')
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+    
+}
+window.addEventListener('scroll',revealFooterBl2)
+revealFooterBl2()
+//Bl3
+function revealFooterBl3(){
+    let reveals = document.querySelectorAll('.FooterContent2Title')
+    for(let i = 0; i < reveals.length;i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 10;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add('active')
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+    
+}
+window.addEventListener('scroll',revealFooterBl3)
+revealFooterBl3()
+//Bl4
+function revealFooterBl4(){
+    let reveals = document.querySelectorAll('.FooterContent2Text')
+    for(let i = 0; i < reveals.length;i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 10;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add('active')
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+    
+}
+window.addEventListener('scroll',revealFooterBl4)
+revealFooterBl4()
+//Bl5
+function revealFooterBl5(){
+    let reveals = document.querySelectorAll('.FooterMin1')
+    for(let i = 0; i < reveals.length;i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 10;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add('active')
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+    
+}
+window.addEventListener('scroll',revealFooterBl5)
+revealFooterBl5()
+//Bl6
+function revealFooterBl6(){
+    let reveals = document.querySelectorAll('.FooterMin2')
+    for(let i = 0; i < reveals.length;i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 10;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add('active')
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+    
+}
+window.addEventListener('scroll',revealFooterBl6)
+revealFooterBl6()
+//Bl7
+function revealFooterBl7(){
+    let reveals = document.querySelectorAll('.FooterMin3')
+    for(let i = 0; i < reveals.length;i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 10;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add('active')
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+    
+}
+window.addEventListener('scroll',revealFooterBl7)
+revealFooterBl7()
+//Bl8
+function revealFooterBl8(){
+    let reveals = document.querySelectorAll('.FooterContent2Btn')
+    for(let i = 0; i < reveals.length;i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 10;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add('active')
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+    
+}
+window.addEventListener('scroll',revealFooterBl8)
+revealFooterBl8()
+//Bl9
+function revealFooterBl9(){
+    let reveals = document.querySelectorAll('.FooterContent2Text2')
+    for(let i = 0; i < reveals.length;i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 10;
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add('active')
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+    
+}
+window.addEventListener('scroll',revealFooterBl9)
+revealFooterBl9()
